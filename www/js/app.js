8acd8dadd08c9b0c73ba10c60580003c7b7b3f18
Jonfor <jonforscher@gmail.com>
2016-07-10 17:37:58 -0400

    Dynamically add to list for search

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 //var angular = require('angular');
-var app = angular.module('bookd', ['ionic', 'bookd.controllers', 'bookd.factories', 'bookd.directives', 'ngCordovaOauth']);
+var app = angular.module('bookd', ['ionic', 'bookd.controllers', 'bookd.factories', 'ngCordovaOauth']);
 //
 //require('./services');
 //require('./controllers');
